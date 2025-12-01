2db0c38f7bbf09254309ded2df5ea1d849bb00c1
woojin-96 <wyunmin0824@gmail.com>
2025-12-01 10:34:37 +0900

    Add files via upload
    FFT 분석기 + 막대 시각화 추가

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 let mySound;
 let started = false;
-
-let fft; 
+let fft;
 
 function preload() {
   soundFormats("mp3");
@@ -10,7 +9,7 @@ function preload() {
 
 function setup() {
   createCanvas(600, 400);
-  fft = new p5.FFT(); 
+  fft = new p5.FFT();
 }
 
 function draw() {
@@ -19,22 +18,23 @@ function draw() {
     fill(255);
     textAlign(CENTER, CENTER);
     textSize(32);
-    text("CLICK TO START", width / 2, height / 2); //시작 화면(오류방지)
+    text("CLICK TO START", width / 2, height / 2); //시작 화면
     return;
   }
 
-  background(50);
-  
-  let spectrum = fft.analyze();
+  background(20);
+
+  let spectrum = fft.analyze(); // 소리 분석
 
   noStroke();
-  fill(250, 200, 0);
+  fill(0, 200, 255);
 
-  for (let i = 0; i < spectrum.length; i += 20) {
-    let h = spectrum[i];
-    rect(i, height - h, 10, h);
+  // 간단한 막대 그래프
+  for (let i = 0; i < spectrum.length; i += 10) {
+    let h = map(spectrum[i], 0, 255, 0, height);
+    rect(i, height - h, 8, h);
   }
-} //음악 반응 확인
+}
 
 function mousePressed() {
   if (!started) {
@@ -42,3 +42,4 @@ function mousePressed() {
     mySound.play();
   }
 }
+
