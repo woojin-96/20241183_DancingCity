3618054dec69ca48fd650111c257a0f297f7b8c8
woojin-96 <wyunmin0824@gmail.com>
2025-12-01 10:57:06 +0900

    사각형에 지붕을 추가해 건물 형태로 바꾸고 색상도 랜덤으로 변하게 함

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-let mySound;
-let started = false;
-let fft;
+let mySound;      
+let started = false; 
+let fft;          
 
 function preload() {
   soundFormats("mp3");
@@ -9,31 +9,50 @@ function preload() {
 
 function setup() {
   createCanvas(600, 400);
-  fft = new p5.FFT();
+  fft = new p5.FFT(); 
+  noStroke();
 }
 
 function draw() {
   if (!started) {
+    // 시작 화면
     background(0);
     fill(255);
     textAlign(CENTER, CENTER);
     textSize(32);
-    text("CLICK TO START", width / 2, height / 2); //시작 화면
-    return;
+    text("CLICK TO START", width / 2, height / 2);
+    return; 
   }
 
-  background(20);
+  background(20); 
+let soundValues = fft.analyze(); 
 
-  let spectrum = fft.analyze(); // 소리 분석
+let spaceBetween = 20;  // 건물 사이 간격
+let buildingWidth = 15; // 건물 폭
 
-  noStroke();
-  fill(0, 200, 255);
-
-  // 간단한 막대 그래프
-  for (let i = 0; i < spectrum.length; i += 10) {
-    let h = map(spectrum[i], 0, 255, 0, height);
-    rect(i, height - h, 8, h);
-  }
+for (let i = 0; i < soundValues.length; i += 5) {
+  
+  // 음악 따라 건물 높이 만들기
+  let buildingHeight = map(soundValues[i], 0, 255, 10, height / 2);
+  
+  // 건물 위치
+  let posX = i * spaceBetween / 5;
+  let posY = height - buildingHeight;
+  
+  // 건물 색상 (음악에 따라 변함)
+  let redColor = map(soundValues[i], 0, 255, 50, 255);
+  let greenColor = map(soundValues[i], 0, 255, 50, 200);
+  let blueColor = map(soundValues[i], 0, 255, 100, 255);
+  fill(redColor, greenColor, blueColor);
+  
+  // 건물 몸통
+  rect(posX, posY, buildingWidth, buildingHeight);
+  
+  // 건물 지붕 (높이에 맞춰서 변함)
+  fill(redColor + 20, greenColor + 20, blueColor + 20);
+  let roofHeight = map(soundValues[i], 0, 255, 5, 20);
+  triangle(posX, posY, posX + buildingWidth, posY, posX + buildingWidth / 2, posY - roofHeight);
+}
 }
 
 function mousePressed() {
@@ -42,4 +61,3 @@ function mousePressed() {
     mySound.play();
   }
 }
-
